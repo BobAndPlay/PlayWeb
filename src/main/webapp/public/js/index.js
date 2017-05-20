ca1dc308ce809eb5ab30a37762644c3032dc5395
bobplay <3141219541@qq.com>
2017-05-20 22:55:11 +0800

    首页轮播图

---
+++ src/main/webapp/public/js/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by zhangpeilei on 2017/5/20.
+ */
+// window.load = function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay : 5000,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+
+
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+
+        // 如果需要滚动条
+        scrollbar: '.swiper-scrollbar',
+    })
+// }
+
+//banner
+
+// index/banners
+$.ajax({
+    url:'index/banners',
+    type:'get',
+    dataType:'json',
+    success:function (data) {
+        console.log(data)
+    }
+
+})
+
