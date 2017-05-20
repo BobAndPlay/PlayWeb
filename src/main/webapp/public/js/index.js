9cd497598fd85be2874a4a279f7b4f5db2ef2c93
peilei520 <3141219541@qq.com>
2017-05-21 01:19:33 +0800

    首页修复轮播图

--- src/main/webapp/public/js/index.js
+++ src/main/webapp/public/js/index.js
@@ -1,7 +1,31 @@
 /**
  * Created by zhangpeilei on 2017/5/20.
  */
-// window.load = function () {
+    $(document).ready(function(){
+
+    })
+
+//banner
+
+// index/banners
+
+    $.ajax({
+        url:'/index/banners',
+        type:'get',
+        dataType:'json',
+        success:function (result) {
+            console.log(result)
+            if(result.code==1){
+                var info =  template("bannerTpl",result);
+                console.log(info);
+                $("#bannerInfo").html(info);
+                slideHandles();
+            }
+        }
+
+    })
+
+function slideHandles(){
     var mySwiper = new Swiper ('.swiper-container', {
         direction: 'horizontal',
         loop: true,
@@ -17,22 +41,4 @@
         // 如果需要滚动条
         scrollbar: '.swiper-scrollbar',
     })
-// }
-
-//banner
-
-// index/banners
-$.ajax({
-    url:'/index/banners',
-    type:'get',
-    dataType:'json',
-    success:function (result) {
-        console.log(result)
-        if(result.code==1){
-           var info =  template("bannerTpl",result.data);
-            $("#bannerInfo").html(info);
-        }
-    }
-
-})
-
+}
